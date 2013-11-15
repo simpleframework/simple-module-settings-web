1e06e4e61ee81e9247cc7638d8ed6058697da0dc
simpleframework <cknet@126.com>
2013-11-15 12:16:54 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/settings/web/plugin/AbstractPreferencesPlugin.java
+++ src/main/java/net/simpleframework/module/settings/web/plugin/AbstractPreferencesPlugin.java
@@ -6,8 +6,7 @@ import net.simpleframework.module.settings.ISettingsPlugin;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885)
- *         https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public abstract class AbstractPreferencesPlugin extends AbstractModulePlugin implements
