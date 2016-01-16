0028f2f8330d0122ec83042a7c09ddd4d709e0b1
CHENK <CHENK@CHENK-PC>
2016-01-16 16:11:55 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/settings/web/ISettingsWebContext.java
+++ src/main/java/net/simpleframework/module/settings/web/ISettingsWebContext.java
@@ -1,7 +1,6 @@
 package net.simpleframework.module.settings.web;
 
 import net.simpleframework.module.settings.ISettingsContext;
-import net.simpleframework.mvc.IMVCContextVar;
 import net.simpleframework.mvc.PageParameter;
 import net.simpleframework.mvc.common.element.AbstractElement;
 
@@ -11,7 +10,7 @@ import net.simpleframework.mvc.common.element.AbstractElement;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public interface ISettingsWebContext extends ISettingsContext, IMVCContextVar {
+public interface ISettingsWebContext extends ISettingsContext {
 
 	/**
 	 * 定义我的偏好设置
